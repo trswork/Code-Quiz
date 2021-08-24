cd803c5484703eb2c9917aaa0ab186b022842934
trswork <87744214+trswork@users.noreply.github.com>
2021-08-23 22:26:42 -0400

    update hover

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
-var startquiz = function (event) {
-    console.log();
-}
+//create the Start Quiz Button
+var startquiz = document.createElement("button");
+    button.innerHTML = "Start Quiz";
 
+//append button
+var body = document.getElementsByClassName("btn-primary");
+btn-primary.appendChild(button);
+
+//add event
+button.addEventListener ("click", function() {
+    alert("start quiz");
+});
+
+//timer
 var seconds = document.getElementById("countdown").textContent;
 var countdown = setInterval(function() {
     seconds--;
