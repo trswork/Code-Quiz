820cdf7ca4e8cf15441979b9aa7f1b56d47f5c50
trswork <87744214+trswork@users.noreply.github.com>
2021-08-28 09:32:20 -0400

    update css

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,46 @@ window.onload = function () {
         display = document.querySelector('#time');
     startTimer(oneMinutes, display);
 };
+
+var questions = [
+    {
+        prompt: "Which tag starts and ends an HTML document?\n(a)HTML,/HTML\n (b) OPEN,/CLOSE\n (c) (HTML),(/HTML)\n (d) StartHTML, endHTML ",
+        answer: "a"
+    },
+    {
+        prompt: "Which of the following is not considered a JavaScript operator?\n\ (a) new\n\ (b) this\n\ (c) delete\n\ (d) typeof",
+        answer: "b"
+    },
+    {
+        prompt: "Which character is used in a closing tag?\n\ (a) &\n\ (b) *\n\ (c) #\n\ (d)/",
+        answer: "d"
+    },
+    {
+        prompt: "What kind of programming language is CSS?\n\ (a) Technique\n\ (b) Style\n\ (c) Order\n\ (d) Hierarchal",
+        answer: "b"
+    },
+    {
+        prompt: "Code that is placed within a title tag will show up where?\n\ (a) Center aligned in the body of a page\n\ (b) At the bottom of a page\n\ (c) In the body of a page\n\ (d)On the title bar in a browser",
+        answer: "d"
+    },
+    {
+        prompt: "In JavaScript, Window.prompt() method return true or false value?\n\ (a) False\n\ (b) True",
+        answer: "a"
+    }
+    
+]
+
+var score = 0;
+
+for (var i = 0; i < questions.length; i++){
+    var response = window.prompt(questions[i].prompt);
+
+    if(response == questions[i].answer){
+        score++;
+        alert("correct");
+    } else {
+        alert("incorrect";)
+    }
+    }
+alert("you got " + score + "/" + questions.length);
+
