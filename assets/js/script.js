090257efe5a493308f12ae5539dc9362dd5fd69a
trswork <87744214+trswork@users.noreply.github.com>
2021-08-28 13:57:53 -0400

    update script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,136 @@
-//create the Start Quiz Button
-var startquiz = document.createElement("button");
-    button.innerHTML = "Start Quiz";
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('queston-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
 
-//append button
-var body = document.getElementsByClassName("btn-primary");
-btn-primary.appendChild(button);
+let shuffledQuestions, currentQuestionIndex
 
-//add event
-button.addEventListener ("click", function() {
-    alert("start quiz");
-});
+startButton.addEventListener('click', startGame)
 
-//timer
-function startTimer(duration, display) {
-    var timer = duration, seconds;
-    setInterval(function () {
-        seconds = parseInt(timer % 60, 10);
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
+function startGame() {
+   console.log('Started')
+   startButton.classList.add('hide')
+   shuffledQuestions = questions.sort(() => Math.random() -.5)
+   currentQuestionIndex = 0
+   questionContainerElement.classList.remove('hide') 
+   setNextQuestion()
 }
 
-window.onload = function () {
-    var oneMinutes = 60 * 1,
-        display = document.querySelector('#time');
-    startTimer(oneMinutes, display);
-};
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])  
+}
 
-var questions = [
-    {
-        prompt: "Which tag starts and ends an HTML document?\n(a)HTML,/HTML\n (b) OPEN,/CLOSE\n (c) (HTML),(/HTML)\n (d) StartHTML, endHTML ",
-        answer: "a"
-    },
-    {
-        prompt: "Which of the following is not considered a JavaScript operator?\n\ (a) new\n\ (b) this\n\ (c) delete\n\ (d) typeof",
-        answer: "b"
-    },
-    {
-        prompt: "Which character is used in a closing tag?\n\ (a) &\n\ (b) *\n\ (c) #\n\ (d)/",
-        answer: "d"
-    },
-    {
-        prompt: "What kind of programming language is CSS?\n\ (a) Technique\n\ (b) Style\n\ (c) Order\n\ (d) Hierarchal",
-        answer: "b"
-    },
-    {
-        prompt: "Code that is placed within a title tag will show up where?\n\ (a) Center aligned in the body of a page\n\ (b) At the bottom of a page\n\ (c) In the body of a page\n\ (d)On the title bar in a browser",
-        answer: "d"
-    },
-    {
-        prompt: "In JavaScript, Window.prompt() method return true or false value?\n\ (a) False\n\ (b) True",
-        answer: "a"
+function showQuestion(questons) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
     
-]
+}
 
-var score = 0;
-
-for (var i = 0; i < questions.length; i++){
-    var response = window.prompt(questions[i].prompt);
-
-    if(response == questions[i].answer){
-        score++;
-        alert("correct");
+function selectAnswer() {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+      nextButton.classList.remove('hide')  
     } else {
-        alert("incorrect";)
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
     }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
     }
-alert("you got " + score + "/" + questions.length);
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    lement.classList.remove('wrong')
+}
+
+const questions = [
+    {
+        question:'Which tag starts and ends an HTML document',
+        answers:  [
+            { text:'HTML,/HTML', correct: true },
+            { text:'OPEN,/CLOSE', correct: false},
+            { text:'(HTML),(/HTML)', correct: false},
+            { text:'StartHTML, endHTML', correct: false}
+        ]
+    },
+    {
+        question:'Which of the following is not considered a JavaScript operator?',
+        answer:  [
+            {text:'new', correct: false },
+            {text:'this', correct: true},
+            {text:'delete', correct: false},
+            {text:'typeof', correct: false}
+        ]
+    },
+    {
+        question:'Which character is used in a closing tag?',
+        answer:  [
+            {text:'&', correct: false },
+            {text:'*', correct: false},
+            {text:'#', correct: false},
+            {text:'/', correct: true}
+        ]
+    },
+    {
+        question:'What kind of programming language is CSS?',
+        answer:  [
+            {text:'Technique', correct: false },
+            {text:'Style', correct: true},
+            {text:'Order', correct: false},
+            {text:'Hierarchal', correct: false}
+        ]
+    },
+    {
+        question:'Code that is placed within a title tag will show up where?',
+        answer:  [
+            {text:'Center aligned in the body of a page', correct: false },
+            {text:'At the bottom of a page', correct: false},
+            {text:'In the body of a page', correct: false},
+            {text:'On the title bar in a browser', correct: true}
+        ]
+    },
+    {
+        question:'In JavaScript, Window.prompt() method return true or false value?',
+        answer:  [
+            {text:'False', correct: true },
+            {text:'True', correct: false},
+        ]
+    }
+]
 
