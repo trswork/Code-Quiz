d9ab1b28ce327674a96dbebc8b48f7e23639aec1
trswork <87744214+trswork@users.noreply.github.com>
2021-08-29 21:30:57 -0400

    timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,24 @@
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
+const showResultButton = document.getElementById('showresults-btn')
 const infoContainerElement = document.getElementById('info-container')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
-
+const resultScreenElement = document.getElementById('resultScreen')
+const resultboxElement = document.getElementById('resultBox');
 let shuffledQuestions, currentQuestionIndex
+
+let index = 0;
+let score = 0;
+let wrong = 0;
 
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
   currentQuestionIndex++
   setNextQuestion()
 })
+showResultButton.addEventListener('click', endGame)
 
 function startGame() {
     startButton.classList.add('hide')
@@ -21,12 +28,10 @@ function startGame() {
     questionContainerElement.classList.remove('hide')
     setNextQuestion(question)
   }
-
   function setNextQuestion() {
     resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
   }
-
   function showQuestion(question) {
     questionElement.innerText = question.question
     question.answers.forEach(answer => {
@@ -40,7 +45,6 @@ function startGame() {
       answerButtonsElement.appendChild(button)
     })
   }
-
   function resetState() {
     clearStatusClass(document.body)
     nextButton.classList.add('hide')
@@ -48,7 +52,6 @@ function startGame() {
       answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
   }
-
   function selectAnswer(e) {
     const selectedButton = e.target
     const correct = selectedButton.dataset.correct
@@ -58,12 +61,27 @@ function startGame() {
     })
     if (shuffledQuestions.length > currentQuestionIndex + 1) {
       nextButton.classList.remove('hide')
-    } else {
+    } else if (endGame) {
       startButton.innerText = 'Restart'
       startButton.classList.remove('hide')
+    } else {
+        showResultButton.innerText = 'Show Results'
+        showResultButton.classList.remove('hide')
     }
   }
-
+  function checkAnswer(option) {
+    score++;
+    let optionClicked = $(option).data("opt");
+    // console.log(questions[index]);
+    if (optionClicked == questions[index].answer) {
+        $(option).addClass("right");
+        score++;
+    } else {
+        $(option).addClass("wrong");
+        wrong++;
+    }
+    $(".scoreBox span").text(score);
+}
   function setStatusClass(element, correct) {
     clearStatusClass(element)
     if (correct) {
@@ -72,12 +90,10 @@ function startGame() {
       element.classList.add('wrong')
     }
   }
-
 function clearStatusClass(element) {
   element.classList.remove('correct')
   element.classList.remove('wrong')
 }
-
 const questions = [
     {
         question: 'Which tag starts and ends an HTML document?',
@@ -133,5 +149,97 @@ const questions = [
       },
 ]
 
-//timer/
-var total_seconds =60*10;
+
+$(function () {
+    // timer code start from here
+
+    let totaTime = 45; // 45 seconds for timer
+    let min = 0;
+    let sec = 0;
+    let counter = 0;
+
+    let timer = setInterval(function () {
+        counter++;
+        min = Math.floor((totaTime - counter) / 60); // calculating min
+        sec = totaTime - min * 60 - counter;
+
+        $(".timerBox span").text(min + ":" + sec);
+
+        if (counter == totaTime) {
+            alert("See Results");
+            result();
+            clearInterval(timer);
+        }
+    }, 1000); // timer set for 1 seconds interval
+    // timer code end here
+
+    // print Question
+    printQuestion(index);
+});
+
+// Function to print question start
+function printQuestion(i) {
+    $(".questionBox").text(questions[i].question);
+    $(".optionBox span").eq(0).text(questions[i].option[0]);
+    $(".optionBox span").eq(1).text(questions[i].option[1]);
+    $(".optionBox span").eq(2).text(questions[i].option[2]);
+    $(".optionBox span").eq(3).text(questions[i].option[3]);
+}
+// Function to print question end
+
+// Function to check answer start
+
+function checkAnswer(option) {
+    attempt++;
+
+    let optionClicked = $(option).data("opt");
+
+    // console.log(questions[index]);
+
+    if (optionClicked == questions[index].answer) {
+        $(option).addClass("right");
+        score++;
+    } else {
+        $(option).addClass("wrong");
+        wrong++;
+    }
+
+    $(".scoreBox span").text(score);
+
+}
+
+// Function to check answer end
+function endGame() {
+    showResultsButton.classList.remove('hide')
+    questionContainerElement.classList.add('hide')
+    resultScreenElement.classList.remove('hide')
+    resultboxElement.classList.remove(hide)
+    showResult()
+  }
+
+function showResult(j) {
+    if (
+        j == 1 &&
+        index < questions.length - 1 &&
+        !confirm(
+            "Quiz has not finished yet. Press ok to skip quiz & get you final result."
+        )
+    ) {
+        return;
+    }
+    result();
+}
+
+// Function for result end
+
+// Result function start
+function result() {
+    $("#questionScreen").hide();
+    $("#resultScreen").show();
+
+    $("#totalQuestion").text(totalQuestion);
+    $("#attemptQuestion").text(attempt);
+    $("#correctAnswers").text(score);
+    $("#wrongAnswers").text(wrong);
+}
+// Result function end
